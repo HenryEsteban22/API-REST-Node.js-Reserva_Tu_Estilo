520600ef1398b233fa3860d2ff1033293c28e84e
HenryEsteban22 <hemc2003@gmail.com>
2026-03-10 00:50:31 -0500

    Integración de la logica de negocio

--- src/routes/citaRoutes.js
+++ src/routes/citaRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import * as citaController from '../controllers/citaController.js';
 import { verificarToken } from '../middlewares/authMiddleware.js';
 
+router.get('/disponibles', verificarToken, citaController.obtenerDisponibilidad);
 router.post('/agendar', verificarToken, citaController.agendarCita);
 router.patch('/:id/confirmar', verificarToken, (req, res) => { 
     req.body.estado = 'confirmada'; 
